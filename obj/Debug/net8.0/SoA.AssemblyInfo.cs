a064546292faeb13deed0d7fd4d2804f2d0fbbac
Roman2423 <void.knight2007@gmail.com>
2025-01-23 16:24:45 +0200

    Changed

--- obj/Debug/net8.0/SoA.AssemblyInfo.cs
+++ obj/Debug/net8.0/SoA.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SoA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78850f1da2bbc91e040b111b360e607b5411efad")]
 [assembly: System.Reflection.AssemblyProductAttribute("SoA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SoA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
